6a82de852c9cf814ece36c2e75102c22ee163963
YeshaGeek123 <yeshageek435@gmail.com>
2022-09-19 19:05:04 +0530

    19-9-22

---
+++ template-parts/content-common.php
@@ -0,0 +1,57 @@
+<section class="main-review">
+    <div class="sec-wp">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-3">
+                    <div class="review-img-wp">
+                        <div class="back-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/09/review-img.jpg');"></div>
+                    </div>
+                </div>
+                <div class="col-lg-9">
+                    <div class="review-content white-text">
+                        <h2 class="h2-title">What our clients say about us</h2>
+                        <div class="review-slider white-text">
+                            <div class="review-slider-content">
+                                <div class="review-text overflow-text" data-simplebar="init">
+                                    <p>After checking out other manufactured home dealers I settled on Caris-Sell. I am in the Snohomish area and it required that I travel to Tri-Cities to conduct business. This I do not regret. I was able to get exactly the home I wanted with several custom features.
+                                        A special thank you to Mariana who was so attentive to my needs and made sure everything was correct. These guys know what they are doing. Kathy, Jim, and April have been a pleasure to do business with. I highly recommend them if you are in the market for a manufactured home.</p>
+                                </div>
+                                <div class="review-by">
+                                    <p>- Dave Hume</p>
+                                </div>
+                            </div>
+                            <div class="review-slider-content">
+                                <div class="review-text overflow-text" data-simplebar="init">
+                                    <p>After checking out other manufactured home dealers I settled on Caris-Sell. I am in the Snohomish area and it required that I travel to Tri-Cities to conduct business. This I do not regret. I was able to get exactly the home I wanted with several custom features.
+                                        A special thank you to Mariana who was so attentive to my needs and made sure everything was correct. These guys know what they are doing. Kathy, Jim, and April have been a pleasure to do business with. I highly recommend them if you are in the market for a manufactured home.</p>
+                                </div>
+                                <div class="review-by">
+                                    <p>- Dave Hume</p>
+                                </div>
+                            </div>
+                            <div class="review-slider-content">
+                                <div class="review-text overflow-text" data-simplebar="init">
+                                    <p>After checking out other manufactured home dealers I settled on Caris-Sell. I am in the Snohomish area and it required that I travel to Tri-Cities to conduct business. This I do not regret. I was able to get exactly the home I wanted with several custom features.
+                                        A special thank you to Mariana who was so attentive to my needs and made sure everything was correct. These guys know what they are doing. Kathy, Jim, and April have been a pleasure to do business with. I highly recommend them if you are in the market for a manufactured home.</p>
+                                </div>
+                                <div class="review-by">
+                                    <p>- Dave Hume</p>
+                                </div>
+                            </div>
+                            <div class="review-slider-content">
+                                <div class="review-text overflow-text" data-simplebar="init">
+                                    <p>After checking out other manufactured home dealers I settled on Caris-Sell. I am in the Snohomish area and it required that I travel to Tri-Cities to conduct business. This I do not regret. I was able to get exactly the home I wanted with several custom features.
+                                        A special thank you to Mariana who was so attentive to my needs and made sure everything was correct. These guys know what they are doing. Kathy, Jim, and April have been a pleasure to do business with. I highly recommend them if you are in the market for a manufactured home.</p>
+                                </div>
+                                <div class="review-by">
+                                    <p>- Dave Hume</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ template-parts/content-home.php
@@ -0,0 +1,181 @@
+<!-- banner start -->
+<section class="main-banner back-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/09/banner-img.jpg');">
+    <div class="sec-wp">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="banner-content white-text text-center">
+                        <h1 class="h1-title">Invest in a Better Manufactured Home </h1>
+                        <div class="banner-btn">
+                            <a href="" class="sec-btn">Call now</a>
+                            <a href="" class="sec-btn btn-transparent">Contact us</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- banner end -->
+
+<!-- about-us start -->
+
+<section class="main-about-us">
+    <div class="sec-wp">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6">
+                    <div class="about-us-img-wp">
+                        <div class="back-img" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/09/about-us-img.jpg');"></div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="about-us-content white-text">
+                        <span class="small-text">ABOUT US</span>
+                        <h2 class="h2-title">Caris-Sell Homes is a family-owned and full-service manufactured home retailer located in Kennewick, WA.</h2>
+                        <div class="about-us-text overflow-text" data-simplebar="init">
+                            <p>We showcase flexible and sustainable home designs that offer comfort and convenience, while retaining the beauty of a traditional American home. We are the “one-stop shop” for those who are interested in owning their own homes. Being in the business for over 20 years, we are here to help you through every step of the way. From choosing the right home to obtaining permits—even finding lenders who know our industry for quicker loan approvals. </p>
+
+                            <p>We also coordinate all site work including site inspections, foundations, steps, electricity, well, and septic systems. What we do is all-inclusive under one roof; Caris-Sell Homes is a company that will serve you from start to finish!</p>
+
+                            <p>Since 1996, our goal has been to exceed our customers’ expectations and offer the highest quality homes and floor plans at the most affordable prices possible. We are a proud retailer of Palm Harbor Homes which has received Manufacturer of the Year for 7 years in a row nationwide.</p>
+
+                            <p>We also carry Champion Homebuilders. For over 60 years Champion has had an established reputation for quality and award-winning designs. With our partners, we’re here to help you make your new home-buying journey easy and attainable.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- about-us end -->
+
+<!-- main-featured-home css start -->
+
+<section class="main-featured-home">
+    <div class="sec-wp">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5">
+                    <div class="featured-home-content">
+                        <span class="small-text">Floor plans</span>
+                        <h2 class="h2-title">Featured Homes</h2>
+                        <div class="featured-home-text">
+                            <p>Buying a home is an exciting milestone in your life.
+                                And with Caris-Sell Homes, you’ll find great quality homes at a fraction of the cost of their traditional counterparts. Each home has been designed specifically for comfort, safety, and convenience.
+                            </p>
+                            <p>We offer modern, energy-efficient, and aesthetically appealing housing solutions that you can enjoy for years to come.</p>
+                        </div>
+                        <div class="featured-home-btn">
+                            <a href="" class="sec-btn">Call now</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-7">
+                    <div class="floor-plan-slider-wp">
+                        <div class="floor-plans-slider">
+                            <div class="floor-plan-card" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/09/about-us-img.jpg');">
+                                <div class="floor-plan-slider-content white-text">
+                                    <h3 class="h3-title">La Belle</h3>
+                                    <span class="sub-title">4 Bedrooms, 3 Baths</span>
+                                    <div class="floor-plan-text overflow-text" data-simplebar="init">
+                                        <p>The La Belle is designed by Palm Harbor Homes.
+                                            It is one of the nation’s leading builders of manufactured homes and they take pride in their superior focus on construction quality. </p>
+                                        <p>The La Belle is a 3,078-square-foot home with 4 bedrooms and 3 baths.</p>
+                                    </div>
+                                </div>
+                                <div class="read-more-wp">
+                                    <span class="text"> READ MORE</span>
+                                    <span class="icon"></span>
+                                </div>
+                            </div>
+                            <div class="floor-plan-card" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/09/about-us-img.jpg');">
+                                <div class="floor-plan-slider-content white-text">
+                                    <h3 class="h3-title">La Belle</h3>
+                                    <span class="sub-title">4 Bedrooms, 3 Baths</span>
+                                    <div class="floor-plan-text overflow-text" data-simplebar="init">
+                                        <p>The La Belle is designed by Palm Harbor Homes.
+                                            It is one of the nation’s leading builders of manufactured homes and they take pride in their superior focus on construction quality. </p>
+                                        <p>The La Belle is a 3,078-square-foot home with 4 bedrooms and 3 baths.</p>
+                                    </div>
+                                </div>
+                                <div class="read-more-wp">
+                                    <span class="text"> READ MORE</span>
+                                    <span class="icon"></span>
+                                </div>
+                            </div>
+                            <div class="floor-plan-card" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/09/about-us-img.jpg');">
+                                <div class="floor-plan-slider-content white-text">
+                                    <h3 class="h3-title">La Belle</h3>
+                                    <span class="sub-title">4 Bedrooms, 3 Baths</span>
+                                    <div class="floor-plan-text overflow-text" data-simplebar="init">
+                                        <p>The La Belle is designed by Palm Harbor Homes.
+                                            It is one of the nation’s leading builders of manufactured homes and they take pride in their superior focus on construction quality. </p>
+                                        <p>The La Belle is a 3,078-square-foot home with 4 bedrooms and 3 baths.</p>
+                                    </div>
+                                </div>
+                                <div class="read-more-wp">
+                                    <span class="text"> READ MORE</span>
+                                    <span class="icon"></span>
+                                </div>
+                            </div>
+                            <div class="floor-plan-card" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/09/about-us-img.jpg');">
+                                <div class="floor-plan-slider-content white-text">
+                                    <h3 class="h3-title">La Belle</h3>
+                                    <span class="sub-title">4 Bedrooms, 3 Baths</span>
+                                    <div class="floor-plan-text overflow-text" data-simplebar="init">
+                                        <p>The La Belle is designed by Palm Harbor Homes.
+                                            It is one of the nation’s leading builders of manufactured homes and they take pride in their superior focus on construction quality. </p>
+                                        <p>The La Belle is a 3,078-square-foot home with 4 bedrooms and 3 baths.</p>
+                                    </div>
+                                </div>
+                                <div class="read-more-wp">
+                                    <span class="text"> READ MORE</span>
+                                    <span class="icon"></span>
+                                </div>
+                            </div>
+                            <div class="floor-plan-card" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/09/about-us-img.jpg');">
+                                <div class="floor-plan-slider-content white-text">
+                                    <h3 class="h3-title">La Belle</h3>
+                                    <span class="sub-title">4 Bedrooms, 3 Baths</span>
+                                    <div class="floor-plan-text overflow-text" data-simplebar="init">
+                                        <p>The La Belle is designed by Palm Harbor Homes.
+                                            It is one of the nation’s leading builders of manufactured homes and they take pride in their superior focus on construction quality. </p>
+                                        <p>The La Belle is a 3,078-square-foot home with 4 bedrooms and 3 baths.</p>
+                                    </div>
+                                </div>
+                                <div class="read-more-wp">
+                                    <span class="text"> READ MORE</span>
+                                    <span class="icon"></span>
+                                </div>
+                            </div>
+                            <div class="floor-plan-card" style="background-image: url('<?php echo home_url(); ?>/wp-content/uploads/2022/09/about-us-img.jpg');">
+                                <div class="floor-plan-slider-content white-text">
+                                    <h3 class="h3-title">La Belle</h3>
+                                    <span class="sub-title">4 Bedrooms, 3 Baths</span>
+                                    <div class="floor-plan-text overflow-text" data-simplebar="init">
+                                        <p>The La Belle is designed by Palm Harbor Homes.
+                                            It is one of the nation’s leading builders of manufactured homes and they take pride in their superior focus on construction quality. </p>
+                                        <p>The La Belle is a 3,078-square-foot home with 4 bedrooms and 3 baths.</p>
+                                    </div>
+                                </div>
+                                <div class="read-more-wp">
+                                    <span class="text"> READ MORE</span>
+                                    <span class="icon"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- main-featured-home css end -->
+
+<?php
+			get_template_part( 'template-parts/content', 'common' );		
+		?>
+
